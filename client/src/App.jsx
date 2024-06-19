43ba0af5cbae0d6a5436859cfa0975f853de240b
chen <565306120@qq.com>
2024-06-19 10:30:04 +0930

    create PostPage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,12 @@ import Projects from './pages/Projects';
 import SignUp from './pages/SignUp';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import PrivateRoute from './components/PrivateRoute';
+import OnlyAdminPrivateRoute from './components/OnlyAdminPrivateRoute';
+import CreatePost from './pages/CreatePost';
+import UpdatePost from './pages/UpdatePost';
+import PostPage from './pages/PostPage';
+
 
 export default function App() {
   return (
